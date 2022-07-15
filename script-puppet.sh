05ccd12915ec4107f9f69749fc2f87c59c283121
hiteshh47 <48388003+hiteshh47@users.noreply.github.com>
2022-07-15 18:56:51 -0500

    Update script-puppet.sh

--- script-puppet.sh
+++ script-puppet.sh
@@ -1,6 +1,5 @@
-curl -O https://apt.puppetlabs.com/puppet6-release-bionic.deb
-sudo dpkg -i puppet6-release-bionic.deb
-sudo apt-get update
-sudo apt-get install puppetserver -y
+curl -O https://apt.puppetlabs.com/puppet-release-bionic.deb
+sudo dpkg -i puppet-release-bionic.deb
+sudo apt-get install puppetmaster -y
 sudo ufw allow 8140
 sudo systemctl enable puppetserver.service
